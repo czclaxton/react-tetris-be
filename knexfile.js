e850ebb59ec1ed783fb5fc3fb0b78899bb61dd78
Connor Claxton <czclaxton@gmail.com>
2020-05-14 16:09:46 -0500

    update

--- knexfile.js
+++ knexfile.js
@@ -21,18 +21,18 @@ module.exports = {
   },
   seeds: { directory: "./seeds" },
 
-  // production: {
-  //   client: "pg",
-  //   connection: process.env.DATABASE_URL,
-  //   useNullAsDefault: true,
-  //   pool: {
-  //     min: 2,
-  //     max: 10,
-  //   },
-  //   migrations: {
-  //     tableName: "dbmigrations",
-  //     directory: "./migrations",
-  //   },
-  //   seeds: { directory: "./seeds" },
-  // },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "dbmigrations",
+      directory: "./migrations",
+    },
+    seeds: { directory: "./seeds" },
+  },
 };
